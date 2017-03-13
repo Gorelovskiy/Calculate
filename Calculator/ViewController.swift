365fb331895de90cc072a9dc805c56adb87de044
Сергей Гореловский <gorelovskiy15@mail.ru>
2017-03-13 19:36:08 +0300

    Create action for button and send information about action in ViewPresenter

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,23 +1,76 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Владимир on 12.03.17.
-//  Copyright © 2017 Gorelovskiy. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+// Add protocol UpdateInformation
+class ViewController: UIViewController, WorkWithView {
+    
+    var presenter: CalculatePresenter!
+    
 
+
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let presenter  = ViewPresenter(view: self)
+        self.presenter = presenter
+        
+    }
+    //  function work under protocol UpdateInformation
+    func updateLabelResult(result: String) {
+        labelResult.text = result
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Actions
+    @IBAction func clearLabel(_ sender: UIButton) {
+    }
+    //
+    @IBAction func buttonDiv(_ sender: UIButton) {
+         presenter.mathOperation(operation: 4)
+    }
+    @IBAction func buttonMuln(_ sender: UIButton) {
+         presenter.mathOperation(operation: 3)
+    }
+    @IBAction func buttonSub(_ sender: UIButton) {
+         presenter.mathOperation(operation: 2)
+    }
+    @IBAction func buttonAdd(_ sender: UIButton) {
+        presenter.mathOperation(operation: 1)
+    }
+    @IBAction func buttonEqually(_ sender: UIButton) {
+    }
+    //
+    @IBAction func button1(_ sender: UIButton) {
+        presenter.tapButton(tap: 1)
+    }
+    @IBAction func button2(_ sender: UIButton) {
+        presenter.tapButton(tap: 2)
+    }
+    @IBAction func button3(_ sender: UIButton) {
+        presenter.tapButton(tap: 3)
+    }
+    @IBAction func button4(_ sender: UIButton) {
+        presenter.tapButton(tap: 4)
+    }
+    @IBAction func button5(_ sender: UIButton) {
+        presenter.tapButton(tap: 5)
+    }
+    @IBAction func button6(_ sender: UIButton) {
+        presenter.tapButton(tap: 6)
+    }
+    @IBAction func button7(_ sender: UIButton) {
+        presenter.tapButton(tap: 7)
+    }
+    @IBAction func button8(_ sender: UIButton) {
+        presenter.tapButton(tap: 8)
+    }
+    @IBAction func button9(_ sender: UIButton) {
+        presenter.tapButton(tap: 9)
+    }
+    @IBAction func button0(_ sender: UIButton) {
+        presenter.tapButton(tap: 0)
+    }
+    //
+    @IBAction func buttonComma(_ sender: UIButton) {
     }
 
 
